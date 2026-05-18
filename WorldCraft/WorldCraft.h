990d14da0a8f51add6fe944f6468115a5b602722
Neil Beresford <neil.beresford@blackstaramps.com>
2026-05-18 09:59:01 +0100

    First include

---
+++ WorldCraft/WorldCraft.h
@@ -0,0 +1,15 @@
+﻿// WorldCraft.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <cmath>
+#include <string>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
